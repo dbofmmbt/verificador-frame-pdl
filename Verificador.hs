9183bd2ac3192487cfb16c43f47356cacced2bc7
Eduardo Canellas de Oliveira <eduardocanellas@id.uff.br>
2019-11-11 21:59:47 -0300

    iteracao basica feita, falta garantir que supre todos os cenários

--- Verificador.hs
+++ Verificador.hs
@@ -6,6 +6,7 @@ module Verficador
 import Arvore
 import Grafo
 import Data.List
+import Data.Maybe
 
 type Modelo = Grafo
 type PDL = Arvore
@@ -19,16 +20,28 @@ verificaRec :: Modelo -> PDL -> Estado -> Bool
 verificaRec m p e
   | p == Vazia = True
   | ehFolha p = verticeTemPrograma v (simbolo p)
+  | simbolo (esquerdo p) == "*" = not $ null $ filter (\valor -> valor) (map (verificaRec m (direito p)) iteracoes)
   | simbolo p == ";" = verificaRec m (esquerdo p) e && verificaRec m (direito p) (proximoEstado v p)
   | simbolo p == "U" = verificaRec m (esquerdo p) e || verificaRec m (direito p ) e
+  -- | simbolo p == "*" = 
   | otherwise = error "Não implementado ainda"
-  where v = m !! e
+  where
+    v = m !! e
+    iteracoes = takeWhile (/=(0-1)) (map (executaIteracao m (esquerdo $ esquerdo p) e) [0..])
 
 proximoEstado :: Vertice -> PDL -> Estado
 proximoEstado v p
   | ehFolha p = fst $ head $ filter (\aresta -> (snd aresta) == (simbolo p)) v
   | simbolo p == ";" = proximoEstado v (esquerdo p)
   | simbolo p == "U" = error "a função \"proximoEstado\" não deveria ser chamada para \"U\""
+  | otherwise = error "caso nao tratado"
+
+executaIteracao :: Modelo -> PDL -> Estado -> Int -> Estado
+executaIteracao m p e i
+  | i == 0 = e
+  | verificaRec m p e = executaIteracao m p (proximoEstado v p) (i-1)
+  | otherwise = -1
+  where v = m !! e
 
 -- testes (só copiar e colar no ghci):
 
@@ -50,3 +63,10 @@ proximoEstado v p
 
 -- alpha; alpha U alpha; beta
 -- verificaFramePDL [[(0, "alpha")], [(0, "beta")]] (criaArvore "Arvore \"U\" (Arvore \";\" (Arvore \"alpha\" Vazia Vazia) (Arvore \"alpha\" Vazia Vazia)) (Arvore \";\" (Arvore \"alpha\" Vazia Vazia) (Arvore \"beta\" Vazia Vazia))")
+
+
+-- alpha*; beta
+-- verificaFramePDL [[(1, "alpha"), (1, "beta")], [(0, "beta")]] (criaArvore "Arvore \";\" (Arvore \"*\" (Arvore \"alpha\" Vazia Vazia) Vazia) (Arvore \"beta\" Vazia Vazia)")
+
+-- alpha*;beta
+-- verificaFramePDL [[(1, "alpha")], [(2, "beta")], []] (criaArvore "Arvore \";\" (Arvore \"*\" (Arvore \"alpha\" Vazia Vazia) Vazia) (Arvore \"beta\" Vazia Vazia)")
